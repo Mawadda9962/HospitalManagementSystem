11171b4a45410862b6ffde9fde0cdd4e6ffe50c3
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-10 09:48:27 +0400

    Adding the Attributes of the person class

--- src/Entities/Person.java
+++ src/Entities/Person.java
@@ -4,5 +4,11 @@ public class Person {
     private String id;
     private  String firstName;
     private  String lastName;
+    private  LocalDate dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
 
 }

2f66cf8c92ec64a8219c49494ee73ddfdcfd16e7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-10 09:54:06 +0400

    Adding the attributes of the patient object

--- src/Entities/Patient.java
+++ src/Entities/Patient.java
@@ -2,9 +2,15 @@ package Entities;
 
 public class Patient extends Person{
 
-
+    private String patientId;
+    private
 
     public Patient(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
     }
+
+
+
+
+}
 }

88869aeb3100143260611d60557081195e55133d
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-10 12:28:15 +0400

    Implementing the constructor

--- src/Entities/Department.java
+++ src/Entities/Department.java
@@ -11,8 +11,15 @@ public class Department {
     private int bedCapacity;
     private int availableBeds;
 
-
-
+    public Department(String departmentName, String departmentId, String headDoctorId, List<Doctor> doctors, List<Nurse> nurses, int bedCapacity, int availableBeds) {
+        this.departmentName = departmentName;
+        this.departmentId = departmentId;
+        this.headDoctorId = headDoctorId;
+        this.doctors = doctors;
+        this.nurses = nurses;
+        this.bedCapacity = bedCapacity;
+        this.availableBeds = availableBeds;
+    }
 
 
 }

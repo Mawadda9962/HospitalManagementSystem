92590deb254d63f68e9570ea4e98eb714711c9ce
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-10 09:52:26 +0400

    Creating a new class

---
+++ src/Entities/Patient.java
@@ -0,0 +1,10 @@
+package Entities;
+
+public class Patient extends Person{
+
+
+
+    public Patient(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+    }
+}

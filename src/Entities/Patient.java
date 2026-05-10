198cc6d90b86a0774892a366599acba0dec6829f
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-10 09:54:54 +0400

    Adding the attributes of the patient object

--- src/Entities/Patient.java
+++ src/Entities/Patient.java
@@ -1,9 +1,13 @@
 package Entities;
 
+import java.util.List;
+
 public class Patient extends Person{
 
     private String patientId;
     private String bloodGroup;
+    private List emergencyContact;
+
 
     public Patient(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);

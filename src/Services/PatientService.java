0bff9ef2cbb3fdc8ba1f7e20143ee95b6ccef851
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-10 14:15:34 +0400

    Adding a static list to store all patients

--- src/Services/PatientService.java
+++ src/Services/PatientService.java
@@ -1,7 +1,15 @@
 package Services;
 
+import Entities.patient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class PatientService {
 
+    private static List<patient> patients = new ArrayList<>();
 
-nb
+
+
 }

9616c5592642ad8b5efca2b4da9ca9adbbf6b59e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-10 12:05:13 +0400

    Adding the attributes of the class

--- src/Entities/Nurse.java
+++ src/Entities/Nurse.java
@@ -1,11 +1,17 @@
 package Entities;
 
+import java.util.List;
+
 public class Nurse extends Person {
 
     private String nurseId;
     private String departmentId;
     private String shift;
     private String qualification;
+    private List assignedPatients;
+
+
+
 
 
 

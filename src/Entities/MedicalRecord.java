e79b2827a9eaacb2489c60fc10f69141b5dbaa6e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-10 12:12:26 +0400

    Creating new class by name String

--- src/Entities/MedicalRecord.java
+++ src/Entities/MedicalRecord.java
@@ -3,6 +3,7 @@ package Entities;
 public class MedicalRecord {
     private String recordId;
     private String patientId;
-    private String
+    private String doctorId;
+    private LocalDate visitDate
 
 }

33a983b9e0406e64f6271ce2fac8be8a77a7382e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-11 16:46:35 +0400

    implementing the constructor

--- src/Entities/Surgeon.java
+++ src/Entities/Surgeon.java
@@ -7,6 +7,12 @@ public class Surgeon extends Doctor{
 
     private int surgeriesPerformed;
     private List surgeryTypes;
-    private boolean operationTheatreAccess
+    private boolean operationTheatreAccess;
 
+    public Surgeon(String doctorId, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients, int surgeriesPerformed, List surgeryTypes, boolean operationTheatreAccess) {
+        super(doctorId, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.surgeriesPerformed = surgeriesPerformed;
+        this.surgeryTypes = surgeryTypes;
+        this.operationTheatreAccess = operationTheatreAccess;
+    }
 }

e42539c607f6f5deeafecf90be76887f08046e9f
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-11 11:50:16 +0400

    Modifying the method of modifying all patients

--- src/Utiles/Constant.java
+++ src/Utiles/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
     public static String INSURANCE_UPDATED_SUCCESSFULLY = "Insurance Updated Successfully";
     public static String PATIENT_ADDED_SUCCESSFULLY = "Patient added Successfully";
     public static String PATIENT_UPDATED_SUCCESSFULLY = "Patient Updated Successfully";
-    public static String PATIENT_REMOVE_SUCCESSFULLY = "Patient Updated Successfully";
+    public static String PATIENT_REMOVE_SUCCESSFULLY = "Patient Removed Successfully";
     public static String NO_PATIENT_REGISTERED = "No patients registered in the system";
     public static String PATIENT_UPDATED = "Patient Updated Successfully";
     public static String DOCTOR_ADDED_SUCCESSFULLY = "Doctor Added Successfully";

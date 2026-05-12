e70fcf63e9eeecc9be471cb4a311f19a6bc3148b
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:51:21 +0400

    Adding EmergencyPatient attributes

--- src/Entities/EmergencyPatient.java
+++ src/Entities/EmergencyPatient.java
@@ -1,10 +1,21 @@
 package Entities;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class EmergencyPatient extends InPatient {
     private String emergencyType;
     private String arrivalMode;
     private int triageLevel;
-    p
+    private boolean admittedViaER;
+
+    public EmergencyPatient(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients, String patientId, String bloodGroup, List<String> emergencyContact, LocalDate registrationDate, String insuranceId, List<String> medicalRecords, List<String> appointments, LocalDate admissionDate, LocalDate dischargeDate, String roomNumber, String bedNumber, String admittingDoctorId, double dailyCharges, String emergencyType, String arrivalMode, int triageLevel, boolean admittedViaER) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients, patientId, bloodGroup, emergencyContact, registrationDate, insuranceId, medicalRecords, appointments, admissionDate, dischargeDate, roomNumber, bedNumber, admittingDoctorId, dailyCharges);
+        this.emergencyType = emergencyType;
+        this.arrivalMode = arrivalMode;
+        this.triageLevel = triageLevel;
+        this.admittedViaER = admittedViaER;
+    }
 
 
 }

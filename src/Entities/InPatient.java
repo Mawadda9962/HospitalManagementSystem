06b4a6716dd3e564f926014f94726bdc8a4a96ba
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:39:17 +0400

    Adding InPatient class

--- src/Entities/InPatient.java
+++ src/Entities/InPatient.java
@@ -19,5 +19,53 @@ public class InPatient {
         this.dailyCharges = dailyCharges;
     }
 
+    public LocalDate getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(LocalDate admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getDischargeDate() {
+        return dischargeDate;
+    }
+
+    public void setDischargeDate(LocalDate dischargeDate) {
+        this.dischargeDate = dischargeDate;
+    }
+
+    public String getBedNumber() {
+        return bedNumber;
+    }
+
+    public void setBedNumber(String bedNumber) {
+        this.bedNumber = bedNumber;
+    }
+
+    public double getDailyCharges() {
+        return dailyCharges;
+    }
+
+    public void setDailyCharges(double dailyCharges) {
+        this.dailyCharges = dailyCharges;
+    }
+
+    public String getAdmittingDoctorId() {
+        return admittingDoctorId;
+    }
+
+    public void setAdmittingDoctorId(String admittingDoctorId) {
+        this.admittingDoctorId = admittingDoctorId;
+    }
+
 
 }

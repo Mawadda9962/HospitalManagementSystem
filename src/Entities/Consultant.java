d14a20f45b5c4b8c35dc98ad7e6756b84d3b5dc1
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 15:54:33 +0400

    Implementing Interfaces in Appropriate Classes

--- src/Entities/Consultant.java
+++ src/Entities/Consultant.java
@@ -1,8 +1,10 @@
 package Entities;
 
+import Interfaces.Displayable;
+
 import java.util.List;
 
-public class Consultant extends Doctor {
+public class Consultant extends Doctor implements Displayable {
     private List<String> consultationTypes;
     private boolean onlineConsultationAvailable;
     private int consultationDuration;
@@ -41,15 +43,43 @@ public class Consultant extends Doctor {
 
     @Override
     public void displayInfo() {
-        System.out.println(" Personal Details");
-        System.out.println("Name: " + getFirstName() + " " + getLastName());
+        System.out.println("ID : " + getId() );
+        System.out.println("First Name: " + getFirstName() );
+        System.out.println("Last Name: " + getLastName() );
+        System.out.println("Date of birth: " + getDateOfBirth() );
+        System.out.println("Gender: " + getGender() );
+        System.out.println("Phone Number: " + getPhoneNumber() );
+        System.out.println("Email: " + getEmail() );
+        System.out.println("Address: " + getAddress());
+        System.out.println("doctor Id : " + getDoctorId() );
+        System.out.println("specialization : " + getSpecialization() );
+        System.out.println("qualification : " + getQualification() );
+        System.out.println("experience Years: " + getExperienceYears() );
+        System.out.println("department Id : " + getDepartmentId());
+        System.out.println("consultation Fee: " + getConsultationFee());
+        System.out.println("available Slots : " + getAvailableSlots());
+        System.out.println("assigned Patients : " + getAssignedPatients());
+        System.out.println("consultation Types : " + getConsultationTypes());
+        System.out.println("consultation Duration : " + getConsultationDuration());
+    }
+
+    @Override
+    public void displaySummary() {
+
 
     }
+
+
     public void scheduleConsultation(){
-
+        System.out.println("Scheduling a new consultation with Dr. " + getLastName());
+        System.out.println("Standard Duration: " + consultationDuration + " minutes.");
+        System.out.println("Available Types: " + consultationTypes);
 
     }
-    public void provideSecondOpinion(){
+    public void provideSecondOpinion(String caseDetails){
+        System.out.println("Consultant Dr. " + getLastName() + " is providing a second opinion.");
+        System.out.println("Case Details Reviewed: " + caseDetails);
+        System.out.println("Status: Finalizing Medical Report...");
 
 
     }

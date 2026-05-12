18b027cdd739cacd847cbe746ea8495919390294
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:26:23 +0400

    Adding GeneralPractitioner class extends Doctor

--- src/Entities/GeneralPractitioner.java
+++ src/Entities/GeneralPractitioner.java
@@ -1,4 +1,8 @@
 package Entities;
 
 public class GeneralPractitioner {
+    private boolean walkinAvailable;
+    private boolean homeVisitAvailable;
+    private boolean vaccinationCertified;
+
 }

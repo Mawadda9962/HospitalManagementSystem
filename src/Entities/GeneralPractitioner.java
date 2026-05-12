4da582b978367748fb8f5b4b250772fdf257b81e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:27:33 +0400

    Adding GeneralPractitioner class extends Doctor

--- src/Entities/GeneralPractitioner.java
+++ src/Entities/GeneralPractitioner.java
@@ -1,10 +1,18 @@
 package Entities;
 
+import java.util.List;
+
 public class GeneralPractitioner extends Doctor{
     private boolean walkinAvailable;
     private boolean homeVisitAvailable;
     private boolean vaccinationCertified;
 
+    public GeneralPractitioner(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients, boolean walkinAvailable, boolean homeVisitAvailable, boolean vaccinationCertified) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.walkinAvailable = walkinAvailable;
+        this.homeVisitAvailable = homeVisitAvailable;
+        this.vaccinationCertified = vaccinationCertified;
+    }
 
 
 

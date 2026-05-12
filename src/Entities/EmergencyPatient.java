137b37c3ef2993351e0ecc618b74ba79078b99a7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:50:36 +0400

    Adding EmergencyPatient attributes

--- src/Entities/EmergencyPatient.java
+++ src/Entities/EmergencyPatient.java
@@ -2,7 +2,9 @@ package Entities;
 
 public class EmergencyPatient extends InPatient {
     private String emergencyType;
-
+    private String arrivalMode;
+    private int triageLevel;
+    p
 
 
 }

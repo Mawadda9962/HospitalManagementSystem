c535ea1513e6f69bc4ee323b98f43b24b9b8b4d3
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:04:14 +0400

    surgeon extends doctors

--- src/Entities/Surgeon.java
+++ src/Entities/Surgeon.java
@@ -2,10 +2,11 @@ package Entities;
 
 import java.util.List;
 
-public class Surgeon {
+public class Surgeon extends Doctor{
     private int surgeriesPerformed;
     private List<String> surgeryTypes;
     private boolean operationTheatreAccess;
+
 
 
 

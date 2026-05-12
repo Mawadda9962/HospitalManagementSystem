13a9908f12a156c2834aa6ffc275c97e78cdcc67
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:21:53 +0400

    Adding a Consultant new class

--- src/Entities/Consultant.java
+++ src/Entities/Consultant.java
@@ -1,4 +1,19 @@
 package Entities;
 
-public class Consultant {
+import java.util.List;
+
+public class Consultant extends Doctor {
+    private List<String> consultationTypes;
+    private boolean onlineConsultationAvailable;
+    private int consultationDuration;
+
+
+    public Consultant(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients, List<String> consultationTypes, boolean onlineConsultationAvailable, int consultationDuration) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.consultationTypes = consultationTypes;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+        this.consultationDuration = consultationDuration;
+    }
+
+
 }

450e0e3980dbaef62354212bf905fc86a58168e8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:38:56 +0400

    Adding InPatient class

--- src/Entities/InPatient.java
+++ src/Entities/InPatient.java
@@ -10,5 +10,14 @@ public class InPatient {
     private String admittingDoctorId;
     private double dailyCharges;
 
+    public InPatient(LocalDate dischargeDate, LocalDate admissionDate, String roomNumber, String bedNumber, String admittingDoctorId, double dailyCharges) {
+        this.dischargeDate = dischargeDate;
+        this.admissionDate = admissionDate;
+        this.roomNumber = roomNumber;
+        this.bedNumber = bedNumber;
+        this.admittingDoctorId = admittingDoctorId;
+        this.dailyCharges = dailyCharges;
+    }
+
 
 }

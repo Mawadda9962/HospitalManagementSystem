694ee9f9fc92ea21fc085b5481233796993117e8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:18:13 +0400

    Adding a Consultant new class

--- src/Entities/Surgeon.java
+++ src/Entities/Surgeon.java
@@ -38,9 +38,20 @@ public class Surgeon extends Doctor{
         this.operationTheatreAccess = operationTheatreAccess;
     }
 
-    public Surgeon(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String doctorId, String specialization, String qualification, int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients) {
-        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+    @Override
+    public void displayInfo() {
+        // Person info (Name, Phone, etc.)
+        System.out.println(" Personal Details");
+        System.out.println("Name: " + getFirstName() + " " + getLastName());
+
     }
 
+    public void performSurgery() {
+
+        }
+
+    public void updateSurgeryCount(){
+
+    }
 
 }

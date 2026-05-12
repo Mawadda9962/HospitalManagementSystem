f54d3cdf1a4e4c42c1e5433d28d2394cda5840be
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-12 09:40:04 +0400

    Adding InPatient class

--- src/Entities/InPatient.java
+++ src/Entities/InPatient.java
@@ -10,62 +10,7 @@ public class InPatient {
     private String admittingDoctorId;
     private double dailyCharges;
 
-    public InPatient(LocalDate dischargeDate, LocalDate admissionDate, String roomNumber, String bedNumber, String admittingDoctorId, double dailyCharges) {
-        this.dischargeDate = dischargeDate;
-        this.admissionDate = admissionDate;
-        this.roomNumber = roomNumber;
-        this.bedNumber = bedNumber;
-        this.admittingDoctorId = admittingDoctorId;
-        this.dailyCharges = dailyCharges;
-    }
 
-    public LocalDate getAdmissionDate() {
-        return admissionDate;
-    }
-
-    public void setAdmissionDate(LocalDate admissionDate) {
-        this.admissionDate = admissionDate;
-    }
-
-    public String getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setRoomNumber(String roomNumber) {
-        this.roomNumber = roomNumber;
-    }
-
-    public LocalDate getDischargeDate() {
-        return dischargeDate;
-    }
-
-    public void setDischargeDate(LocalDate dischargeDate) {
-        this.dischargeDate = dischargeDate;
-    }
-
-    public String getBedNumber() {
-        return bedNumber;
-    }
-
-    public void setBedNumber(String bedNumber) {
-        this.bedNumber = bedNumber;
-    }
-
-    public double getDailyCharges() {
-        return dailyCharges;
-    }
-
-    public void setDailyCharges(double dailyCharges) {
-        this.dailyCharges = dailyCharges;
-    }
-
-    public String getAdmittingDoctorId() {
-        return admittingDoctorId;
-    }
-
-    public void setAdmittingDoctorId(String admittingDoctorId) {
-        this.admittingDoctorId = admittingDoctorId;
-    }
 
 
 }

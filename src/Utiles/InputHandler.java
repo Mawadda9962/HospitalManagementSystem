2e592a29dec3a1165f59085a1dfcd472fb86bfd0
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-13 14:21:09 +0400

    Creating an InputHandler class

--- src/Utiles/InputHandler.java
+++ src/Utiles/InputHandler.java
@@ -6,5 +6,21 @@ public class InputHandler {
 
     private static Scanner scanner = new Scanner(System.in);
 
+    public static String getStringInput(String prompt){
+        while(true){
+            System.out.println(prompt);
+            String input = scanner.nextLine();
+            if (HelperUtils.isValidString(input)){
+                return input;
+            }
+            System.out.println("Please Enter a Valid String");
+        }
+    }
+
+    public static int getIntInput(String prompt){
+
+    }
+
+
 
 }

52ec3f8ce39062913aaa57f11b89ce8feaab21d1
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-05-14 10:01:25 +0400

    Adding a new class of the Sample Data esting

--- src/Utiles/SampleDataTesting.java
+++ src/Utiles/SampleDataTesting.java
@@ -1,6 +1,8 @@
 package Utiles;
 
 import Entities.Department;
+import Entities.EmergencyPatient;
+import Entities.InPatient;
 import Entities.OutPatient;
 import Services.*;
 
@@ -23,71 +25,62 @@ public class SampleDataTesting {
         departmentService.add(new Department("Emergency", "DEP-04", "DOC-104", new ArrayList<>(), new ArrayList<>(), 50, 50));
 
         InPatient ip1 = new InPatient(
-                "P300",                 // id
-                "Salim",                // firstName
-                "Al-Rawahi",            // lastName
-                "1975-08-20",           // dateOfBirth (String)
-                "Male",                 // gender
-                "92228877",             // phoneNumber
-                "salim.r@example.om",   // email
-                "Ibra",                 // address
-                "N/A",                  // specialization
-                "N/A",                  // qualification
-                0,                      // experienceYears
-                "DEP-01",               // departmentId
-                0.0,                    // consultationFee
-                null,          // availableSlots
-                null,     // assignedPatients
-                "P300",                 // patientId
-                "B+",                   // bloodGroup
-                null,    // emergencyContact (List)
-                LocalDate.now(),        // registrationDate
-                "OMAN-INS-99",          // insuranceId
-                null,       // medicalRecords
-                null,         // appointments
-                LocalDate.of(2026, 5, 1), // admissionDate
-                LocalDate.of(2026, 5, 10),// dischargeDate
-                "Room-105",             // roomNumber
-                "Bed-A2",               // bedNumber
-                "DOC-101",              // admittingDoctorId
-                25.5                    // dailyCharges
+                "P300", "Salim", "Al-Rawahi", "1975-08-20", "Male",
+                "92228877", "salim.r@example.om", "Ibra", "N/A", "N/A", 0,
+                "DEP-01", 0.0, null, null, "P300", "B+", null,
+                LocalDate.now(), "OMAN-INS-99", null, null,
+                LocalDate.of(2026, 5, 1), LocalDate.of(2026, 5, 10),
+                "Room-105", "Bed-A2", "DOC-101", 25.5
         );
-
-        //Add to the service
         patientService.add(ip1);
 
 
+
         OutPatient op2 = new OutPatient(
-                "P200",               // id
-                "Fatma",              // firstName
-                "Al-Zadjali",         // lastName
-                "1992-11-05",         // dateOfBirth (String)
-                "Female",             // gender
-                "95556677",           // phoneNumber
-                "fatma@example.om",   // email
-                "Muscat",             // address
-                "General",            // specialization
-                "MBBS",               // qualification
-                0,                    // experienceYears
-                "DEP-01",             // departmentId
-                0.0,                  // consultationFee
-                null,          // availableSlots
-                null,     // assignedPatients
-                "P200",               // patientId
-                "O-",                 // bloodGroup
-                null,    // emergencyContact
-                LocalDate.now(),      // registrationDate
-                "OmanIns-102",        // insuranceId
-                null,           // medicalRecords
-                null,                // appointments
-                5,                    // visitCount
-                LocalDate.of(2026, 4, 10), // lastVisitDate
-                "DOC-101"             // preferredDoctorId
+                "P200", "Fatma", "Al-Zadjali", "1992-11-05", "Female",
+                "95556677", "fatma@example.om", "Muscat", "General", "MBBS", 0,
+                "DEP-01", 0.0, null, null, "P200", "O-", null,
+                LocalDate.now(), "OmanIns-102", null, null,
+                5, LocalDate.of(2026, 4, 10), "DOC-101"
         );
-        //Register the patient in your Service
-        patientService.add(op1);
+        patientService.add(op2);
 
-
+        // This uses your 32-parameter constructor matching the EmergencyPatient class
+        EmergencyPatient ep1 = new EmergencyPatient(
+                "P400",                 // id
+                "Ahmed",                // firstName
+                "Al-Balushi",           // lastName
+                "1988-03-12",           // dateOfBirth
+                "Male",                 // gender
+                "93334455",                 // phoneNumber
+                "ahmed.b@hospital.om",  // email
+                "Sohar",                // address
+                "N/A",                  // specialization
+                "N/A",                  // qualification
+                0,                      // experienceYears
+                "DEP-04",               // departmentId (Emergency)
+                0.0,                    // consultationFee
+                null,                   // availableSlots
+                null,                   // assignedPatients
+                "P400",                 // patientId
+                "A-",                   // bloodGroup
+                null,                   // emergencyContact
+                LocalDate.now(),        // registrationDate
+                "OMAN-GOV-01",          // insuranceId
+                null,                   // medicalRecords
+                null,                   // appointments
+                LocalDate.now(),        // admissionDate
+                null,                   // dischargeDate (Still in ER)
+                "ER-Room-01",           // roomNumber
+                "ER-Bed-05",            // bedNumber
+                "DOC-104",              // admittingDoctorId
+                50.0,                   // dailyCharges
+                "Heart Attack",         // emergencyType
+                "Ambulance",            // arrivalMode
+                1,                      // triageLevel (Level 1 = Critical)
+                true                    // admittedViaER
+        );
+        patientService.add(ep1);
 
 
     }
